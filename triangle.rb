8f5095885de7cce9df706a45afe75e563d2add3d
ozim <ozim10@gmail.com>
2016-01-28 22:57:14 +0100

    161/282 - triangle project 2 completed

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if (a <= 0 || b <= 0 || c <= 0)
+  raise TriangleError if (a + b <= c || a + c <= b || b + c <= a)
   return :equilateral if (a == b  && a == c)
   return :isosceles if (a == b || a == c || b == c)
   return :scalene if (a != b && a != c && b != c)
